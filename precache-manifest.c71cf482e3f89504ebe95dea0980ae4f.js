b792d5108e89005199db2d022a6729c249a413c5
Madison Ofarril <mofarril.mof@gmail.com>
2019-12-15 11:44:38 -0500

    Updates

---
+++ precache-manifest.c71cf482e3f89504ebe95dea0980ae4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36db2a62f1109987e9607d5666aefaf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a9588afd439c947eb13",
+    "url": "/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "e8d678511fabd88d43a4",
+    "url": "/static/css/main.b4079242.chunk.css"
+  },
+  {
+    "revision": "4a9588afd439c947eb13",
+    "url": "/static/js/2.9739d0a2.chunk.js"
+  },
+  {
+    "revision": "e8d678511fabd88d43a4",
+    "url": "/static/js/main.2a6dd1e2.chunk.js"
+  },
+  {
+    "revision": "4a7002a455db355720e8",
+    "url": "/static/js/runtime-main.60d0d36f.js"
+  }
+]);
