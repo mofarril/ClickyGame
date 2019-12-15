b8471bb31a0928e3a0aa938f2a56e57eb22ccbca
Madison Ofarril <mofarril.mof@gmail.com>
2019-12-15 11:39:34 -0500

    trying to deploy

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 //import registerServiceWorker from './registerServiceWorker';
 
 // If you want your app to work offline and load faster, you can change
