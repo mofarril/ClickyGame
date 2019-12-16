2182b90a389d70c36970a53f6636e10e62ba657d
Madison Ofarril <mofarril.mof@gmail.com>
2019-12-16 18:23:00 -0500

    changed homepage

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-const ghpages = require ('gh-pages');
-ghpages.publish('dist', function (err){});
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
